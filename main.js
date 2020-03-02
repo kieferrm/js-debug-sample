5f1b9ef2183cf0c0bb2610646f6b616206328fe1
kieferrm <kieferrm@gmail.com>
2020-03-02 23:35:51 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+/** @type {HTMLInputElement} */
+const first = document.querySelector('#first');
+/** @type {HTMLInputElement} */
+const second = document.querySelector('#second');
+const result = document.querySelector('#result');
+
+const worker = new Worker("worker.js");
+
+first.onchange = function() {
+  debugger; // Step _into_ the next line
+  worker.postMessage([first.value, second.value]);
+}
+
+second.onchange = function() {
+  debugger; // Step _into_ the next line
+  worker.postMessage([first.value, second.value]);
+}
+
+worker.onmessage = function(e) {
+  // You are back in main! Check out the CALL STACK.
+  // Try selecting threads in the THREADS view.
+  result.textContent = e.data;
+}
